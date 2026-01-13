08cb3a33cdea3de5c3eaab7bad66ddd4758c263b
Yuvanjeyan <yuvanyvgs5454@gmail.com>
2026-01-13 11:55:08 +0530

    Fix responsive layout for Mobile screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,12 @@
 function Navbar({ cartCount, openCart }) {
-    return (
-        <nav className="navbar">
-            <h1>Cart Application</h1>
-            <button onClick={openCart}>
-                Cart ({cartCount})
-            </button>
-
-        </nav>
-    )
-
+  return (
+    <nav className="navbar">
+      <h1 className="navbar-title">Cart Application</h1>
+      <button className="cart-btn" onClick={openCart}>
+        Cart ({cartCount})
+      </button>
+    </nav>
+  );
 }
+
 export default Navbar;
